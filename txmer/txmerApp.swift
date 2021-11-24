6ab2a29ca1dfdddfe28e6b9df5a873464de7b34d
Tim Xie <pdt.xie@gmail.com>
2021-11-24 15:33:09 +1300

    Initial Commit

---
+++ txmer/txmerApp.swift
@@ -0,0 +1,20 @@
+//
+//  txmerApp.swift
+//  txmer
+//
+//  Created by Tim Xie on 24/11/21.
+//
+
+import SwiftUI
+
+@main
+struct txmerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
