b89d205ec23d91bab60546f467a3a5c3d54eb0a3
Reagan Bohan <xbjfk.github@gmail.com>
2021-11-25 22:22:34 +1300

    tim is idot he put sessions in stats view like an idot

--- txmer/StatsView.swift
+++ txmer/StatsView.swift
@@ -1,5 +1,5 @@
 //
-//  StatsView.swift
+//  SessionsView.swift
 //  txmer
 //
 //  Created by macos sucks balls on 11/25/21.
@@ -9,46 +9,7 @@ import SwiftUI
 
 struct StatsView: View {
     var body: some View {
-        NavigationView {
-            ZStack {
-                Color(UIColor.systemGray6) /// todo make so user can change colour/changes dynamically with system theme - but when dark mode, change systemgray6 -> black (or not full black >:C)
-                    .ignoresSafeArea()
-                
-                ScrollView() {
-                    VStack (spacing: 16) {
-                        
-                        Button(action: {
-                            print("hi")
-                        }) {
-                            Text("hi")
-                                .font(.system(size: 17, weight: .bold, design: .default))
-                                .foregroundColor(Color.black)
-                                .frame(width: UIScreen.screenWidth - 32, height: 100)
-                                
-                                .background(Color.white)
-                                .cornerRadius(10)
-                        }
-                        
-                        Button(action: {
-                            print("hi")
-                        }) {
-                            Text("hi")
-                                .font(.system(size: 17, weight: .bold, design: .default))
-                                .foregroundColor(Color.black)
-                                .frame(width: UIScreen.screenWidth - 32, height: 100)
-                                
-                                .background(Color.white)
-                                .cornerRadius(10)
-                        }
-                    }
-                }
-                .navigationTitle("Your Sessions")
-            }
-        }
-        
-        
-        
-        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
