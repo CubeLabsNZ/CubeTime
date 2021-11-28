9e585a6326ad11b804b602a7f8f1eb3dca62b6ac
Tim Xie <pdt.xie@gmail.com>
2021-11-28 21:55:51 +1300

    very beta ui for new session

--- txmer/SessionsView.swift
+++ txmer/SessionsView.swift
@@ -29,15 +29,15 @@ struct NewStandardSessionView: View {
                 if let error = error as NSError? {
                     // Replace this implementation with code to handle the error appropriately.
                     // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
+                    
                     /*
-                    Typical reasons for an error here include:
-                    * The parent directory does not exist, cannot be created, or disallows writing.
-                    * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                    * The device is out of space.
-                    * The store could not be migrated to the current model version.
-                    Check the error message to determine what the actual problem was.
-                    */
+                     Typical reasons for an error here include:
+                     * The parent directory does not exist, cannot be created, or disallows writing.
+                     * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                     * The device is out of space.
+                     * The store could not be migrated to the current model version.
+                     Check the error message to determine what the actual problem was.
+                     */
                     fatalError("Unresolved error \(error), \(error.userInfo)")
                 }
             }
@@ -59,46 +59,93 @@ struct NewSessionPopUpView: View {
     
     @Binding var showNewSessionPopUp: Bool
     
+    init(showNewSessionPopUp: Binding<Bool>) {
+        //UITableView.appearance().separatorStyle = .none
+        UITableViewCell.appearance().backgroundColor = .white
+        UITableView.appearance().backgroundColor = .systemGray6
+    }
+    
     var body: some View {
         VStack {
-            Button("go back") {
-                dismiss()
-            }
-            Text("Add New Session")
-            Text("You can choose from four different types of sessions, out of the following: ")
-            
             NavigationView {
-                List {
-                    Section(header: Text("Normal Sessions")) {
-                        NavigationLink(
-                            "next",
-                            destination: NewStandardSessionView(showNewSessionPopUp: $showNewSessionPopUp),
-                            isActive: $showNewSessionView)
+                VStack {
+                    HStack {
+                        Spacer()
                         
+                        Button {
+                            print("new session view closed")
+                            dismiss()
+                        } label: {
+                            Image(systemName: "xmark.circle.fill")
+                                .font(.system(size: 22, weight: .semibold))
+                                .symbolRenderingMode(.hierarchical)
+                                .padding(.top)
+                                .padding(.trailing)
+                        }
+                    }
+                    
+                    
+                    VStack(alignment: .center) {
+                        Text("Add New Session")
+                            .font(.system(size: 34, weight: .bold, design: .default))
+                            .padding(.bottom, 8)
+                            .padding(.top, 36)
+                        Text("You can choose from four different types of sessions, out of the following: ")
+                            .font(.system(size: 17, weight: .regular, design: .default))
+                            .multilineTextAlignment(.center)
+                            .padding(.bottom)
+                    }
+                    
+                    
+                    
+                    
+                    List {
+                        /*
+                        NavigationLink("Standard Session", destination: NewStandardSessionView(showNewSessionPopUp: $showNewSessionPopUp), isActive: $showNewSessionView)
+                        NavigationLink("Algorithm Trainer DO NOT CLICK RN", destination: NewStandardSessionView(showNewSessionPopUp: $showNewSessionPopUp), isActive: $showNewSessionView)
+                        NavigationLink("Playground DO NOT CLICK RN", destination: NewStandardSessionView(showNewSessionPopUp: $showNewSessionPopUp), isActive: $showNewSessionView)
                         
+                        NavigationLink("Comp Sim Mode DO NOT CLICK", destination: NewStandardSessionView(showNewSessionPopUp: $showNewSessionPopUp), isActive: $showNewSessionView)
+                         */
+                        Text("hi")
+                        Text("hi")
+                        Text("hi")
+                        Text("hi")
+                        Text("hi")
                         
+                    }
+                    
+                    Spacer()
+                    
+                }
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarHidden(true)
+                
+                
+                
+            }
+            
+            /*
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        print("new session view closed")
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .font(.system(size: 22, weight: .semibold))
+                            .symbolRenderingMode(.hierarchical)
+                            .padding(.top)
+                            .padding(.trailing)
                         
-                        Text("thing1")
-                        Text("thing1")
                     }
                 }
-                .listStyle(.insetGrouped)
+                    
             }
+             */
             
             
-            List {
-                Section(header: Text("Normal Sessions")) {
-                    Text("thing1")
-                    Text("thing1")
-                    Text("thing1")
-                }
-            }
-            .listStyle(.insetGrouped)
-            
-            
-            
-            Spacer()
-            
+            //Spacer()
         }
     }
 }
@@ -169,32 +216,32 @@ struct SessionsView: View {
                     }
                     
                     /* only keeping because i think this is unpineneds size
-                    VStack {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text("3x3 main session")
-                                    .font(.system(size: 22, weight: .bold, design: .default))
-                                Text("3x3")
-                                    .font(.system(size: 15, weight: .medium, design: .default))
-                            }
-                            
-                            Spacer()
-                            
-                            Image(systemName: "square.fill")
-                                .font(.system(size: 44))
-                                .padding(.trailing, 6)
-                            
-                        }
-                        .padding(.leading)
-                        .padding(.trailing, 4)
-                        .padding(.top, 8)
-                        .padding(.bottom, 8)
-                        
-                    }
-                    .frame(height: 65)
-                    .background(Color(UIColor.white).clipShape(RoundedRectangle(cornerRadius:16)))
-                    .padding(.trailing)
-                    .padding(.leading) */
+                     VStack {
+                     HStack {
+                     VStack(alignment: .leading) {
+                     Text("3x3 main session")
+                     .font(.system(size: 22, weight: .bold, design: .default))
+                     Text("3x3")
+                     .font(.system(size: 15, weight: .medium, design: .default))
+                     }
+                     
+                     Spacer()
+                     
+                     Image(systemName: "square.fill")
+                     .font(.system(size: 44))
+                     .padding(.trailing, 6)
+                     
+                     }
+                     .padding(.leading)
+                     .padding(.trailing, 4)
+                     .padding(.top, 8)
+                     .padding(.bottom, 8)
+                     
+                     }
+                     .frame(height: 65)
+                     .background(Color(UIColor.white).clipShape(RoundedRectangle(cornerRadius:16)))
+                     .padding(.trailing)
+                     .padding(.leading) */
                     
                     
                     Button("+ New Session") {
@@ -202,6 +249,7 @@ struct SessionsView: View {
                     }
                     .sheet(isPresented: $showNewSessionPopUp) {
                         NewSessionPopUpView(showNewSessionPopUp: $showNewSessionPopUp)
+                        //NewSessionPopUpView()
                     }
                 }
                 .navigationTitle("Your Sessions")
