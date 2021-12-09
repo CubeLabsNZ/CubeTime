ae4f8697c9da8677b9d17985fdb375f24cea5bfa
Tim Xie <pdt.xie@gmail.com>
2021-12-09 23:22:42 +1300

    started on settings ui

--- txmer/Settings/SettingsView.swift
+++ txmer/Settings/SettingsView.swift
@@ -9,6 +9,20 @@ import SwiftUI
 import SwiftUICharts
 
 
+struct AnimatingFontSizeV2: AnimatableModifier {
+    var fontSize: CGFloat
+
+    var animatableData: CGFloat {
+        get { fontSize }
+        set { fontSize = newValue }
+    }
+
+    func body(content: Self.Content) -> some View {
+        content
+            .font(.system(size: self.fontSize, weight: .bold))
+    }
+}
+
 @available(iOS 15.0, *)
 struct SettingsView: View {
     @State var currentCard: SettingsCardInfo = settingsCards[0]
@@ -104,7 +118,7 @@ struct SettingsCard: View {
         .background(Color.white.clipShape(RoundedRectangle(cornerRadius: 16)))
         .matchedGeometryEffect(id: "bg " + info.name, in: namespace)
         .onTapGesture {
-            withAnimation {
+            withAnimation(.spring(response: 1)) {
                 currentCard = info
                 showingCard = true
             }
@@ -123,39 +137,81 @@ struct SettingsDetail: View {
             ZStack {
                 Color(UIColor.systemGray6)
                     .ignoresSafeArea()
+                    .zIndex(0)
 
                 VStack {
+                    
+                    ZStack {
+                        Color(UIColor.white)
+                            .ignoresSafeArea()
+                            .clipShape(RoundedRectangle(cornerRadius: 16))
+                            .matchedGeometryEffect(id: "bg " + currentCard.name, in: namespace)
+                            
+                            
+                            
                         
-                    HStack {
-                        Text(currentCard.name)
-                            .font(.system(size: 22, weight: .bold))
-                            .matchedGeometryEffect(id: currentCard.name, in: namespace)
-                        
-                        Spacer()
-                        
-                        Image(systemName: currentCard.icon)
-                            .font(currentCard.iconStyle)
-                            .matchedGeometryEffect(id: currentCard.icon, in: namespace)
+                        VStack {
+                            Spacer()
+                            
+                            HStack {
+                                Text(currentCard.name)
+                                    .matchedGeometryEffect(id: currentCard.name, in: namespace)
+                                
+                                
+                                    
+                                    
+                                
+                                Spacer()
+                                
+                                Image(systemName: currentCard.icon)
+                                    .font(currentCard.iconStyle)
+                                    .matchedGeometryEffect(id: currentCard.icon, in: namespace)
+                            }
+                            .padding()
+                        }
                         
                     }
-                    .background(Color.white)
-                    .matchedGeometryEffect(id: "bg " + currentCard.name, in: namespace)
-                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                    .ignoresSafeArea()
+                    .frame(maxHeight: UIScreen.screenHeight / 6)
                     
+                    Spacer()
                     
                     switch currentCard.name {
                     case "About":
                         AboutSettingsView()
+                            .safeAreaInset(edge: .bottom, spacing: 0) {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .fill(Color.clear)
+                                    .frame(height: 50 + (SetValues.hasBottomBar ? 0 : CGFloat(SetValues.marginBottom)))
+                                    .padding(.top)
+                            }
                     default:
-                        Text("hi")
+                        Text("unable to load view")
+                    }
+                    
+                }
+                .zIndex(1)
+                
+                
+                VStack {
+                    HStack {
+                        Spacer()
+                        
+                        Image(systemName: "xmark.circle.fill")
+                            .padding()
+                            .onTapGesture {
+                                withAnimation(.spring(response: 1)) {
+                                    showingCard = false
+                                }
+                                
+                            }
                     }
                     Spacer()
                 }
+                .zIndex(2)
                 
-                Text("close")
-                    .onTapGesture {
-                        showingCard = false
-                    }
+                    
+                    
                 
             }
         }
