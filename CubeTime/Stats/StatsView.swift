c02555467caa794881f99c240133b97cd85d4133
Reagan Bohan <xbjfk.github@gmail.com>
2021-12-30 23:46:41 +1300

    Save target in comp sim

--- CubeTime/Stats/StatsView.swift
+++ CubeTime/Stats/StatsView.swift
@@ -794,15 +794,10 @@ struct StatsView: View {
                                                 .foregroundColor(Color(uiColor: .systemGray))
                                                 .padding(.bottom, 4)
 
-                                            if bestSingle != nil {
-                                                Text("target")
-                                                    .font(.system(size: 34, weight: .bold, design: .default))
-                                                    .foregroundColor(Color(uiColor: colourScheme == .light ? .black : .white))
-                                            } else {
-                                                Text("N/A")
-                                                    .font(.system(size: 28, weight: .medium, design: .default))
-                                                    .foregroundColor(Color(uiColor: .systemGray5))
-                                            }
+                                            Text(formatSolveTime(secs: (currentSession as! CompSimSession).target, dp: 2))
+                                                .font(.system(size: 34, weight: .bold, design: .default))
+                                                .foregroundColor(Color(uiColor: colourScheme == .light ? .black : .white))
+
                                         }
                                         .padding(.top)
                                         .padding(.bottom, 12)
