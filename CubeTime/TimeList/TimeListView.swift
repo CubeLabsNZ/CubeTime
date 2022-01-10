f2f427a4ca596e7043785f7277a2be90bebef015
Reagan Bohan <xbjfk.github@gmail.com>
2022-01-10 21:12:43 +1300

    fix batch delete solves

--- CubeTime/TimeList/TimeListView.swift
+++ CubeTime/TimeList/TimeListView.swift
@@ -221,12 +221,14 @@ struct TimeListView: View {
                                 NSLog("hi")
                                 for object in selectedSolves {
                                     managedObjectContext.delete(object)
+                                    withAnimation {
+                                        timeListManager.delete(object)
+                                    }
                                 }
                                 selectedSolves.removeAll()
                                 withAnimation {
                                     if managedObjectContext.hasChanges {
                                         try! managedObjectContext.save()
-                                        timeListManager.resort() // TODO optimize
                                     }
                                 }
                             } label: {
