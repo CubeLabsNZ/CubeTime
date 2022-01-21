6e5c7b52996b6060f4f37cd03b908736cad1ab77
Reagan Bohan <xbjfk.github@gmail.com>
2022-01-21 17:41:10 +1300

    Draw scramble in time list

---
+++ CubeTime/ScrambleView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrambleView.swift
+//  CubeTime
+//
+//  Created by macos sucks balls on 1/21/22.
+//
+
+import Foundation
+import SwiftUI
+import SVGKit
+
+
+struct ScrambleImageView: UIViewRepresentable {
+    @Binding var puzzle: OrgWorldcubeassociationTnoodleScramblesPuzzleRegistry
+    @Binding var scramble: String
+    
+    func makeUIView(context: Context) -> SVGKFastImageView {
+        let svgText = JavaUtilObjects.toString(withId: puzzle.getScrambler().drawScramble(with: scramble, with: nil))
+        let svgImage = SVGKImage(data: svgText.data(using: .utf8))
+        return SVGKFastImageView(svgkImage: svgImage!)!
+    }
+    
+    func updateUIView(_ uiView: SVGKFastImageView, context: Context) {
+        let svgText = JavaUtilObjects.toString(withId: puzzle.getScrambler().drawScramble(with: scramble, with: nil))
+        uiView.image = SVGKImage(data: svgText.data(using: .utf8))
+    }
+}
