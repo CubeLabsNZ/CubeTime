8de5704e3b467ded8fc6d5e5f457ceb6fe47f11e
Tim Xie <pdt.xie@gmail.com>
2022-01-31 17:11:27 +1300

    fixed indicator size on scaled scramble view

--- CubeTime/Timer/ScrambleView.swift
+++ CubeTime/Timer/ScrambleView.swift
@@ -48,31 +48,8 @@ struct TimerSVGView: UIViewRepresentable {
     var svg: OrgWorldcubeassociationTnoodleSvgliteSvg
     
     func makeUIView(context: Context) -> SVGKFastImageView {
-        
-        
-        
-        
-//        NSLog("\(UIScreen.main.scale)")
-
-//        let svgsize = CGSize(svg.getSize())
-//        NSLog("svgsize \(svgsize)")
-        
-//        NSLog("width: \(svgsize.width), height: \(svgsize.height)")
-        
-//        let ratio: CGFloat = CGFloat(svgsize.width) / CGFloat(svgsize.height)
-//        NSLog("ratio \(ratio)")
-//        let newsize = CGSize(width: size.width, height: CGFloat(size.width) * ratio)
-//        NSLog("newsize \(newsize)")
-        
-//        let newsize = CGSize(width: 20, height: 10)
-        
-//        svg.setSizeWith(OrgWorldcubeassociationTnoodleSvgliteDimension(newsize))
         let svgstr = JavaUtilObjects.toString(withId: svg)
         let svgImage = SVGKImage(data: svgstr.data(using: .utf8))!
-        
-        
-        
-//        svgImage.size = newsize
         
         return SVGKFastImageView(svgkImage: svgImage)!
     }
@@ -86,10 +63,6 @@ struct TimerSVGView: UIViewRepresentable {
 struct TimerScrambleView: View {
     let svg: OrgWorldcubeassociationTnoodleSvgliteSvg?
     var body: some View {
-        if let svg = svg {
-            TimerSVGView(svg: svg)
-        } else {
-            ProgressView()
-        }
+        TimerSVGView(svg: svg!)
     }
 }
