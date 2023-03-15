9c8043131018523c9a87cf7feef701b3d7a46cb1
Reagan Bohan <xbjfk.github@gmail.com>
2023-03-15 21:54:53 +1300

    fix db name

--- CubeTime/Persistence.swift
+++ CubeTime/Persistence.swift
@@ -20,7 +20,7 @@ struct PersistenceController {
     let container: NSPersistentContainer
 
     init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "txmerdata")
+        container = NSPersistentContainer(name: "CubeTimeData")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
