f10dad567470946805c5bf045dd900bd7afb81fd
Reagan Bohan <xbjfk.github@gmail.com>
2024-02-19 22:01:03 +1300

    Add Export types

--- CubeTime/Sessions/ImportExport.swift
+++ CubeTime/Sessions/ImportExport.swift
@@ -13,39 +13,86 @@ struct ImportFlow: View {
     }
 }
 
-struct ExportFlow: View {
-    @EnvironmentObject var stopwatchManager: StopwatchManager
+struct ExportFlowPickSessions: View {
+    @EnvironmentObject var exportViewModel: ExportViewModel
     
-    @State private var selectedSessions = Set<Session>()
     
-    var sessions: FetchedResults<Session>
+    let sessions: FetchedResults<Session>
+    
     
     var body: some View {
-        VStack {
-            List(sessions, id: \.id, selection: $selectedSessions) { session in
+        ForEach(sessions) { session in
+            let selected = exportViewModel.selectedSessions.contains(session)
+            HStack {
                 Text(session.name ?? "UNKNOWN NAME")
+                Spacer()
+                Image(systemName: selected ? "checkmark.circle.fill" : "checkmark.circle")
             }
-            .navigationTitle("Export Sessions")
-            
-            NavigationLink(destination: ExportSelectExportType()) {
-                CTButton(type: selectedSessions.count == 0 ? .disabled : .halfcoloured(nil), size: .large, onTapRun: { }) {
-                    HStack {
-                        Text("Continue")
-                        
-                        Image(systemName: "arrow.forward")
-                    }
+            .onTapGesture {
+                if selected {
+                    exportViewModel.selectedSessions.remove(session)
+                } else {
+                    exportViewModel.selectedSessions.insert(session)
                 }
             }
-            .frame(maxWidth: .infinity, alignment: .trailing)
-            .padding(.trailing)
         }
-        .environment(\.editMode, .constant(EditMode.active))
-        .safeAreaInset(safeArea: .tabBar)
+        .navigationTitle("Export Sessions")
+        
+        CTButton(type: exportViewModel.selectedSessions.count == 0 ? .disabled : .halfcoloured(nil), size: .large, onTapRun: { exportViewModel.exportFlowState = .pickingFormats }) {
+            HStack {
+                Text("Continue")
+                
+                Image(systemName: "arrow.forward")
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .trailing)
+        .padding(.trailing)
+
     }
 }
 
-struct ExportSelectExportType: View {
+struct ExportFlowPickFormats: View {
+    @EnvironmentObject var exportViewModel: ExportViewModel
+    
     var body: some View {
-        Text("Select")
+        let zippedArray = Array(zip(ExportViewModel.allFormats.indices, ExportViewModel.allFormats))
+        ForEach(zippedArray, id: \.0) { (_, format) in
+//            let selected = exportViewModel.selectedFormats.contains(format)
+            let indexInSelected = exportViewModel.selectedFormats.firstIndex(where: {$0 === format})
+            HStack {
+                Text(format.name)
+                Spacer()
+                Image(systemName: indexInSelected != nil ? "checkmark.circle.fill" : "checkmark.circle")
+            }
+            .onTapGesture {
+                if let indexInSelected {
+                    exportViewModel.selectedFormats.remove(at: indexInSelected)
+                } else {
+                    exportViewModel.selectedFormats.append(format)
+                }
+            }
+        }
+        .navigationTitle("Export Sessions")
+
+    }
+}
+
+struct ExportFlow: View {
+    @EnvironmentObject var stopwatchManager: StopwatchManager
+    @StateObject var exportViewModel: ExportViewModel = ExportViewModel()
+    
+    let sessions: FetchedResults<Session>
+    
+    var body: some View {
+        VStack {
+            switch exportViewModel.exportFlowState {
+            case .pickingSessions:
+                ExportFlowPickSessions(sessions: sessions)
+            case .pickingFormats:
+                ExportFlowPickFormats()
+            }
+        }
+        .safeAreaInset(safeArea: .tabBar)
+        .environmentObject(exportViewModel)
     }
 }
