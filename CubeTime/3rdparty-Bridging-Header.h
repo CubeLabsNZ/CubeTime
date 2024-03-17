a09f4c387759e72f501310c318c085741e9b4454
Reagan Bohan <xbjfk.github@gmail.com>
2024-03-17 23:24:32 +1300

    fix oopsies

--- CubeTime/3rdparty-Bridging-Header.h
+++ CubeTime/3rdparty-Bridging-Header.h
@@ -2,7 +2,7 @@
 #define Bridging_Header_h
 
 #import "graal_isolate.h"
-//#import "org.worldcubeassociation.tnoodle.scrambles.main.h"
+#import "org.worldcubeassociation.tnoodle.scrambles.main.h"
 #import "getBestAverage.h++"
 
 #endif /* Bridging_Header_h */
